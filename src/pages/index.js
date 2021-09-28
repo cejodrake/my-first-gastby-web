95cf9c07435c7f3fb178fd874fbf79f68004f833
Juan <j_Calix2002@hotmail.com>
2021-09-27 23:01:08 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import LayoutPage from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
@@ -8,6 +9,10 @@ const IndexPage = () => {
       <LayoutPage pageTitle="Home Page " />
       <Link to="/about">About</Link>
       <p> Learing Gastby from Documentation</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </main>
   );
 };
